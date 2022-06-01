912a67dc8a211a2d0b99328184ddaea634c84784
Tommy Bonina <kss5zj@virginia.edu>
2022-06-01 16:42:02 -0400

    debugHomePage

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -8,12 +8,15 @@ function HomePage() {
     
     const { accessToken } = useContext(AccessTokenContext);
     const [songs, setSongs] = useState([])
-
+    // const {accessToken} = "BQBGVvGcaL4Usn654_rkbXcNLoJrqdHQuhi7N1wEPP9fip2GKq2_6Nsdyn0w6-7yvidZ9XFuLac-p9C2HgBgEarG7CLukZP7RRbENtKAIBA6mVbML17CQj6cJB35tdIqZShoEw84QBj1aLcM9vTrTm1OmDpcJ2V5BA2fFg_l03r5DXKZbsGWhVz7N4cKkWPXolerRRBsWYQE6AjTba8MCN1iHIKeng";
     useEffect(() => {
-     fetch("/user?token="+ accessToken).then(res => res.json()).then(data => setSongs(data.items))
+     fetch("/user?token=" + "BQAhSZddqFDVYpfhBEya4YSA17zI5aQdxUcVVZixmrSgMxSZ4u004Ry8Yz8xEaLhDWCRaaLDsuDI6T-Fv-DI3ulT6wmso3xDp6Z0uNm2FL7OOudHaa6gqRBNkkyKsGj1ywYVXmZOSpCFwNYX9uFKg4fetukKz1IMMxc").then(res => res.json()).then(data => setSongs(data.items))
     }, [])
-   
-    console.log(songs)
+    // useEffect(() => {
+    //     fetch("/user?token=" + {accessToken}).then(res => res.json()).then(data => setSongs(data.items))
+    //    }, [])
+   console.log("access token", accessToken);
+   console.log(songs)
     return (
         <div>
             <h1>welcome</h1>
