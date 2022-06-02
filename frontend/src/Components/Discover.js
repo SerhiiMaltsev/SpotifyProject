7f9799c174bbc43b9179f510aeefaaa13ac6d41c
Tommy Bonina <kss5zj@virginia.edu>
2022-06-02 14:14:34 -0400

    debug Discover

--- frontend/src/Components/Discover.js
+++ frontend/src/Components/Discover.js
@@ -4,22 +4,23 @@ import Navbar from './Navbar'
 
 const Discover = () => {
 
-    const [users, setUsers] = useState();
-    
+    const [users, setUsers] = useState([]);
+
     useEffect(() =>{
         fetch("http://localhost:9000/getusers")
      .then(res => res.json())
      .then(data => setUsers(data))
     },[])
+
     console.log(users);
     return(
         <div class = "Discover">
             <Navbar />
-            {users.map((user) => {
+            {users.map((user, key) => {
                 return <p>{user.name}</p>
             })}
         </div>
-
     )
+
 }
 export default Discover
