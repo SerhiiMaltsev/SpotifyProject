d81b13129afb586906dd4f593438cdd5fdd81568
SerhiiMaltsev <sm5zj@virginia.edu>
2022-06-02 11:03:19 -0400

    Login backend

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -4,14 +4,16 @@ import { useContext } from 'react';
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
-import { Button, Grid } from '@mui/material';
 import { margin } from '@mui/system';
+import "./Login.css"
+import { AppBar, Toolbar, IconButton, Grid, Typography, Button, Stack} from '@mui/material'
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 function Login() {
 
     const navigate = useNavigate();
     const { accessToken, setAccessToken } = useContext(AccessTokenContext);
-    
+
     const onClick = (e) => {
         fetch("http://localhost:9000/auth").then(res => res.json())
         .then(data => {
@@ -27,22 +29,27 @@ function Login() {
             fetch('http://localhost:9000/auth/callback?code='+code).then(res => res.json()).then(data => {
                 if(data.token){
                     setAccessToken(data.token)
-                    //console.log(data.token)
                     navigate('/home')
                 }
-           //setAccessToken(data.access_token)
-           //setRefreshToken(data.refresh_token)    
-        })
-    }
+            })
+        }
     }, [])
 
     return (
-        <>
-        <Navbar/>
-        <Grid item marginTop={10} marginLeft={71}>
+        <div className="Login">
+          <div className="Navbar">
+          <Toolbar>
+            <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+              <LibraryMusicIcon/>
+              <Typography variant='h6' fontSize={30} sx={{flexGrow: 1}}>Spotify</Typography></IconButton>
+              <IconButton size='large' edge='start' padding ='100'color='inherit' aria-label='logo'>
+            </IconButton>
+          </Toolbar>
+          </div>
+          <div className="LoginButton">
             <Button onClick={(e) => onClick(e)} variant='contained' color='secondary'>Log in to App</Button>
-            </Grid>
-        </>
+          </div>
+        </div>
     )
 }
 
